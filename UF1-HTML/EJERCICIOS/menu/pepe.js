92969d545a1507948759bbee91d5797c65292a1f
carlosCV19 <150705154+carlosCV19@users.noreply.github.com>
2023-12-03 17:03:23 +0100

    Add files via upload

---
+++ UF1-HTML/EJERCICIOS/menu/pepe.js
@@ -0,0 +1,4 @@
+function expandSearch() {
+    var input = document.getElementById('search-input');
+    input.style.width = (input.style.width === '150px' || input.style.width === '0') ? '0' : '150px';
+}
